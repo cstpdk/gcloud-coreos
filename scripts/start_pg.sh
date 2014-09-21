e0efe964472b99c4a02c373b553103128fe1e192
Christoffer Pedersen <stofferpedersen@gmail.com>
2014-09-21 23:01:02 +0200

    Disk for jenkins, new pg image
    Also removed ip from db

--- scripts/start_pg.sh
+++ scripts/start_pg.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -p 5432:5432 postgres
+docker run --name db_data -v /root/postgresql:/var/lib/postgresql wyaeld/postgres:data
+
+docker run -p 5432:5432 --volumes-from db_data \
+	-e POSTGRESQL_USER=quniz -e POSTGRESQL_PASS=quniz \
+	-e POSTGRESQL_DB=quniz wyaeld/postgres:9.3
