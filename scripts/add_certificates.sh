9d183325abf1f024d72476ffc4de8fba8c7c9f9c
Christoffer Pedersen <stofferpedersen@gmail.com>
2014-10-27 11:33:47 +0100

    big whoops, removed debug and destructive stuff from add_certificates

--- scripts/add_certificates.sh
+++ scripts/add_certificates.sh
@@ -1,10 +1,7 @@
 config_file="$1"
 pem_files="${@:2}"
-echo "" > $config_file
 for pem in $pem_files ; do
 	echo -e "  - path: /home/core/keys/$(basename $pem)" >> $config_file
 	echo -e "    content: |\n" >> $config_file
-	echo $pem
-	cat $pem
 	cat $pem >> $config_file
 done
