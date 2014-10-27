1cc2d82ec02f002d2c41155d181f6b73eac57853
Christoffer Pedersen <stofferpedersen@gmail.com>
2014-10-27 11:36:33 +0100

    removed extra space from add_certificates

--- scripts/add_certificates.sh
+++ scripts/add_certificates.sh
@@ -2,6 +2,6 @@ config_file="$1"
 pem_files="${@:2}"
 for pem in $pem_files ; do
 	echo -e "  - path: /home/core/keys/$(basename $pem)" >> $config_file
-	echo -e "    content: |\n" >> $config_file
+	echo -e "    content: |" >> $config_file
 	cat $pem >> $config_file
 done
